f9b008b5ab72c32bd0dfb0e891666849d777e0c1
temi <temitopem@gmail.com>
2019-06-25 16:57:25 -0400

    Initial commit

---
+++ src/com/company/Animalapp.java
@@ -0,0 +1,26 @@
+package com.company;
+public class Animalapp {
+
+    public static void main(String[] args) {
+
+        Animal a = new Animal();
+        print(a.eat());
+        print(a.sleep());
+
+        Cat c = new Cat();
+        print(c.eat());
+        print(c.sleep());
+        print(c.purr());
+
+
+        Bird b = new Bird();
+        print(b.eat());
+        print(b.sleep());
+        print(b.fly());
+
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
+}
